51bcebd86c5431275834b658209be5bb7e0f9cae
krishna-eit <krishna.eitb@gmail.com>
2025-12-10 13:31:33 +0530

    update

--- app/api/assign/route.ts
+++ app/api/assign/route.ts
@@ -31,8 +31,8 @@ import { NextResponse } from "next/server";
 
 // List of members
 const members = [
-  "Krishna", "Umapathi", "Sarswathi", "Hari", "Malini", "Ashwin", "Stalin", 
-  "Jency", "YuvaShree", "Kiruthika", "Rohit", "Revanth", "Nithish", "Arjun", 
+  "Krishna", "Umapathi", "Saraswathi", "Hari", "Malini", "Ashwin", "Stalin", 
+  "Jency", "YuvaShree", "Kiruthika", "Rohit", "Revanth", "Nithish", 
   "Karuniya", "Sharan", "Gunashree", "Saipavan", "Balaji", "Sreenivasulu", "Akash", 
   "Roslin", "Monisha", "Charu", "Sanjith", "Siva", "AshwinKumar", "MuthuKumaran", "SamratAshok","Ilamaran","SivaPratheesh"
 ];
